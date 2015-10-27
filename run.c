ce03bb52c73db5fe1716da23b3ff16a3e01ed658
kchwang <kchwang@cs.nctu.edu.tw>
2015-10-28 05:26:40 +0800

    ii

--- run.c
+++ run.c
@@ -17,17 +17,20 @@ void *tosse(void *threadid)
 
 int main( int argc, char *argv[] )  
 {
-	long int tossesAmount=1;
+	long tossesAmount = 1;
     int rc = 0;
-    long int t = 0;
+    long t = 0;
+    printf("%d",tossesAmount);
    	if( argc == 2 )
    	{
-      	tossesAmount = (long int)argv[1];
+        printf("%d",tossesAmount);
+      	tossesAmount = (long)argv[1];
+        printf("%d",tossesAmount);
         pthread_t threads[tossesAmount];
 
       	for(t=0; t<tossesAmount; t++)
 		{
-      		rc = pthread_create(&threads[t], &attr, tosse, (void *)t);
+      		rc = pthread_create(&threads[t], NULL, tosse, (void *)t);
       		if (rc)
 			{
          		printf("ERROR; return code from pthread_create() is %d\n", rc);
